d8295102fb60f6ac31436a956ce8a9c4ea82217e
mudongliang <mudongliangabcd@gmail.com>
2014-05-13 18:54:58 +0800

    目前我们只是做了一个测试版本的helloOS ，它的功能只是将一串固定字符显示到屏幕上。然而真实的系统需要boot loader。由于我想要完全体验这个过程，所以我们就不用现成的boot loader，而改为自己编写一个小的。那么首先我们决定采用多阶段启动模式。从固态存储设备上启动的Boot Loader 大多采用两阶段，即启动过程可以分为 stage1和stage2：stage1完成初始 化硬件，为stage2准备内存空间，并将stage2复制到内存中，设置堆栈，然后跳转到stage2。对应于我们的启动来说，就是boot将loader加载到内存，设置堆栈，之后将执行权限交给我们的 loader，loader做的工作就是初始化GDT，IDT等，切换保护模式，并将kernel加载到内存中，跳到对应位置执行。 另外，为了防止大家看不懂这方面的知识，我特地为大家准备了很多资料，这些都是我在学习过程中搜集的或者自己总结的。 让操作系统走进保护模式(知识个人总结精华版) 软盘结构 FAT文件系统 FAT文件系统白皮书 nasm命令列表 参考资料书如下： ORANGE’S：一个操作系统的实现 Linux 内核完全剖析——基于0.12内核

--- detailedos/boot.asm
+++ detailedos/boot.asm
@@ -2,6 +2,35 @@ org	07c00h
 ;;它告诉编译器，程序被载入内存的初始偏移地址为0x7c00h，
 ;;因此编译产生的代码中，涉及MOV等指令的标号的偏移量都加上了0x7c00h,
 ;;常量并不会发生变化
+	
+;;关于PBP的详情，请看以下参考资料
+;;让操作系统走进保护模式(知识个人总结精华版)
+;;软盘结构，FAT文件系统，FAT文件系统白皮书
+	jmp short LABEL_START		; Start to boot.
+	nop				; 这个 nop 不可少
+
+	; 下面是 FAT12 磁盘的头
+	BS_OEMName		DB 'MSWIN4.1'	; OEM String, 必须 8 个字节
+	BPB_BytsPerSec	DW 512		; 每扇区字节数
+	BPB_SecPerClus	DB 1		; 每簇多少扇区
+	BPB_RsvdSecCnt	DW 1		; Boot 记录占用多少扇区
+	BPB_NumFATs		DB 2		; 共有多少 FAT 表
+	BPB_RootEntCnt	DW 224		; 根目录文件数最大值
+	BPB_TotSec16	DW 2880		; 逻辑扇区总数
+	BPB_Media		DB 0xF0		; 媒体描述符
+	BPB_FATSz16		DW 9		; 每FAT扇区数
+	BPB_SecPerTrk	DW 18		; 每磁道扇区数
+	BPB_NumHeads	DW 2		; 磁头数(面数)
+	BPB_HiddSec		DD 0		; 隐藏扇区数
+	BPB_TotSec32	DD 0		; wTotalSectorCount为0时这个值记录扇区数
+	BS_DrvNum		DB 0		; 中断 13 的驱动器号
+	BS_Reserved1	DB 0		; 未使用
+	BS_BootSig		DB 29h		; 扩展引导标记 (29h)
+	BS_VolID		DD 0		; 卷序列号
+	BS_VolLab		DB 'MyDetaOS001'; 卷标, 必须 11 个字节
+	BS_FileSysType	DB 'FAT12   '	; 文件系统类型, 必须 8个字节  
+
+LABEL_START:
 	mov 	ax, cs
 	mov 	ds, ax
 	mov 	es, ax
@@ -26,14 +55,15 @@ DispStr:
 ;;串：Char，attr，……，char，attr
 	mov 	ax, BootMessage
 	mov 	bp, ax
-   	mov  	cx, 16
+   	mov  	cx, LengthOfBootMessage	;;这样比硬编码要有健壮性
 	mov 	ax, 01301h
  	mov 	bx, 000ch
 	mov 	dl, 0
 	int 	10h
 	ret
-BootMessage: 	db  	"Hello, OS World!"
-times	510-($-$$)	db	0
+BootMessage: 		db  	"Hello, OS World!"
+LengthOfBootMessage 	equ	$ - BootMessage
+times		510-($-$$)	db	0
 ;;NASM 在表达式中支持两个特殊的记号，即'$'和'$$',它们允许引用当前指令的地址。
 ;;'$'计算得到它本身所在源代码行的开始处的地址；所以你可以简单地写这样的代码'jmp $'来表示无限循环。
 ;;'$$'计算当前段开始处的地址，所以你可以通过($-$$)找出你当前在段内的偏移
