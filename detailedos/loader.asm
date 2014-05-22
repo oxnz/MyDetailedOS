861446552f029569193bd4b2224b0120c8c8b069
mudongliang <mudongliangabcd@gmail.com>
2014-05-22 15:37:53 +0800

    此次提交所做的工作有些多，大家看的时候注意，如下： 1.首先，我们将一些可配置的参数放到了bootloader两个文件之外，方便使用；其次，我们编写了GDT和对应的选择子，这只是暂时的，在正式kernel启动的时候，gdt会再次发生变化；然后我们添加系统进入保护模式的代码，并且进入保护模式之后进行一系列的初始化操作。其中注意一点，进入保护模式的操作有几个部分，分别是加载gdt，关中断，打开地址线A20，将CR0的第一位PE置1.然后跳转到32位代码区执行。 2.其次的工作就是得到系统中内存分布，然后打印出来，最后就是分页的代码，分页的工作只是示范，因为分页的操作使得线性地址=物理地址，相当于什么没做。另外，关于内存的获取，使用的int 15h中断，我单独将此内容列出放入了资料库中。另外，由于bootsector在启动之后是可以覆盖的，所以我将很长时间之后才会修改的内存分布图提前放置到资料库中。

--- detailedos/loader.asm
+++ detailedos/loader.asm
@@ -6,15 +6,37 @@ org  0100h
 ;关于PBP以及软盘文件系统FAT12的详细信息，请看以下参考资料
 ;让操作系统走进保护模式(知识个人总结精华版)
 ;软盘结构，FAT文件系统，FAT文件系统白皮书
-;======================================================================================================
-BaseOfStack			equ	  0100h
-BaseOfKernel		equ	 08000h	; KERNEL.ELF 被加载到的位置 ---- 段地址
-OffsetOfKernel		equ	     0h	; KERNEL.ELF 被加载到的位置 ---- 偏移地址
-;======================================================================================================	
+
 	jmp LABEL_START
 
 ; 下面是 FAT12 磁盘的头, 独立出来的原因是这两个文件都要使用这些参数。
 %include "fat12hdr.inc"
+%include "load.inc"
+%include "pm.inc"
+
+;关于gdt,selector,descriptor等名词的具体含义，请去资料库中寻找赵炯老师和于渊老师的书，
+;书中有详细的解释，方便大家学习
+; GDT
+;                            段基址     段界限, 属性
+LABEL_GDT:	    Descriptor 0,            0, 0              ; 空描述符
+LABEL_DESC_FLAT_C:  Descriptor 0,      0fffffh, DA_CR|DA_32|DA_LIMIT_4K ;0-4G
+LABEL_DESC_FLAT_RW: Descriptor 0,      0fffffh, DA_DRW|DA_32|DA_LIMIT_4K;0-4G
+LABEL_DESC_VIDEO:   Descriptor 0B8000h, 0ffffh, DA_DRW|DA_DPL3 ; 显存首地址
+
+GdtLen		equ	$ - LABEL_GDT
+GdtPtr		dw	GdtLen - 1				; 段界限
+		dd	BaseOfLoaderPhyAddr + LABEL_GDT		; 基地址
+
+; GDT 选择子
+SelectorFlatC		equ	LABEL_DESC_FLAT_C	- LABEL_GDT
+SelectorFlatRW		equ	LABEL_DESC_FLAT_RW	- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT + SA_RPL3
+
+;======================================================================================================
+BaseOfStack			equ	  0100h
+PageDirBase			equ	100000h	; 页目录开始地址: 1M
+PageTblBase			equ	101000h	; 页表开始地址:   1M + 4K
+;======================================================================================================	
 
 LABEL_START:
 	mov	ax, cs
@@ -24,7 +46,25 @@ LABEL_START:
 	mov	sp, BaseOfStack			;初始化各个寄存器，特别是sp
 	
 	mov	dh, 0			; "Loading  "
-	call	DispStr			; 显示字符串
+	call	DispStrRealMode	; 显示字符串
+
+	; 得到内存数
+	mov	ebx, 0			; ebx = 后续值, 开始时需为 0
+	mov	di, _MemChkBuf		; es:di 指向一个地址范围描述符结构(ARDS)
+.MemChkLoop:
+	mov	eax, 0E820h			; eax = 0000E820h
+	mov	ecx, 20			; ecx = 地址范围描述符结构的大小
+	mov	edx, 0534D4150h		; edx = 'SMAP'
+	int	15h				; int 15h
+	jc	.MemChkFail
+	add	di, 20
+	inc	dword [_dwMCRNumber]	; dwMCRNumber = ARDS 的个数
+	cmp	ebx, 0
+	jne	.MemChkLoop
+	jmp	.MemChkOK
+.MemChkFail:
+	mov	dword [_dwMCRNumber], 0
+.MemChkOK:
 
 	xor	ah, ah	
 	xor	dl, dl	
@@ -147,7 +187,28 @@ LABEL_FILE_LOADED:
 	call	KillMotor		; 关闭软驱马达
 
 	mov	dh, 1			; "Ready."
-	call	DispStr			; 显示字符串
+	call	DispStrRealMode	; 显示字符串
+
+	; 下面准备跳入保护模式
+
+	; 加载 GDTR
+	lgdt	[GdtPtr]
+
+	; 关中断
+	cli
+
+	; 打开地址线A20
+	in	al, 92h
+	or	al, 00000010b
+	out	92h, al
+
+	; 准备切换到保护模式
+	mov	eax, cr0
+	or	eax, 1
+	mov	cr0, eax
+
+	; 真正进入保护模式
+	jmp	dword SelectorFlatC:(BaseOfLoaderPhyAddr+LABEL_PM_START)
 
 	jmp $
 
@@ -160,8 +221,8 @@ LABEL_FILE_LOADED:
 ;============================================================================
 ;变量
 wRootDirSizeForLoop	dw	RootDirSectors	; Root Directory 占用的扇区数, 在循环中会递减至零.
-wSectorNo		dw	0	; 要读取的扇区号
-bOdd			db	0		; 奇数还是偶数
+wSectorNo			dw	0	; 要读取的扇区号
+bOdd				db	0		; 奇数还是偶数
 dwKernelSize		dd	0		; KERNEL.ELF 文件大小
 
 ;字符串
@@ -174,10 +235,10 @@ Message2			db	"No KERNEL" 	; 9字节, 不够则用空格补齐. 序号 2
 ;============================================================================
 
 ;----------------------------------------------------------------------------
-; 函数名: DispStr
+; 函数名: DispStrRealMode
 ;----------------------------------------------------------------------------
 ; 功能: 利用BIOS的10h号中断调用在屏幕打印字符串, 函数开始时 dh 中应该是字符串序号(0-based)
-DispStr:
+DispStrRealMode:
 ;10h中断调用 ah=13h
 ;功能：显示字符串
 ;参数：ES:BP = 串地址 
@@ -314,3 +375,347 @@ KillMotor:
 	pop	dx
 	ret
 ;----------------------------------------------------------------------------
+
+; 从此以后的代码在保护模式下执行 ----------------------------------------------------
+; 32 位代码段. 由实模式跳入 ---------------------------------------------------------
+[SECTION .s32]
+
+ALIGN	32
+
+[BITS	32]
+
+LABEL_PM_START:
+	mov	ax, SelectorVideo
+	mov	gs, ax
+
+	mov	ax, SelectorFlatRW
+	mov	ds, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	ss, ax
+	mov	esp, TopOfStack
+
+	push	szMemChkTitle
+	call	DispStr
+	add	esp, 4
+
+	call	DispMemInfo
+	call	SetupPaging
+
+	mov	ah, 0Fh				; 0000: 黑底    1111: 白字
+	mov	al, 'P'
+	mov	[gs:((80 * 0 + 39) * 2)], ax	; 屏幕第 0 行, 第 39 列。
+	jmp	$
+
+; ------------------------------------------------------------------------
+; 显示 AL 中的数字
+; ------------------------------------------------------------------------
+DispAL:
+	push	ecx
+	push	edx
+	push	edi
+
+	mov	edi, [dwDispPos]
+
+	mov	ah, 0Fh			; 0000b: 黑底    1111b: 白字
+	mov	dl, al
+	shr	al, 4
+	mov	ecx, 2
+.begin:
+	and	al, 01111b
+	cmp	al, 9
+	ja	.1
+	add	al, '0'
+	jmp	.2
+.1:
+	sub	al, 0Ah
+	add	al, 'A'
+.2:
+	mov	[gs:edi], ax
+	add	edi, 2
+
+	mov	al, dl
+	loop	.begin
+	;add	edi, 2
+
+	mov	[dwDispPos], edi
+
+	pop	edi
+	pop	edx
+	pop	ecx
+
+	ret
+; DispAL 结束-------------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; 显示一个整形数
+; ------------------------------------------------------------------------
+DispInt:
+	mov	eax, [esp + 4]
+	shr	eax, 24
+	call	DispAL
+
+	mov	eax, [esp + 4]
+	shr	eax, 16
+	call	DispAL
+
+	mov	eax, [esp + 4]
+	shr	eax, 8
+	call	DispAL
+
+	mov	eax, [esp + 4]
+	call	DispAL
+
+	mov	ah, 07h			; 0000b: 黑底    0111b: 灰字
+	mov	al, 'h'
+	push	edi
+	mov	edi, [dwDispPos]
+	mov	[gs:edi], ax
+	add	edi, 4
+	mov	[dwDispPos], edi
+	pop	edi
+
+	ret
+; DispInt 结束------------------------------------------------------------
+
+; ------------------------------------------------------------------------
+; 显示一个字符串
+; ------------------------------------------------------------------------
+DispStr:
+	push	ebp
+	mov	ebp, esp
+	push	ebx
+	push	esi
+	push	edi
+
+	mov	esi, [ebp + 8]	; pszInfo
+	mov	edi, [dwDispPos]
+	mov	ah, 0Fh
+.1:
+	lodsb
+	test	al, al
+	jz	.2
+	cmp	al, 0Ah	; 是回车吗?
+	jnz	.3
+	push	eax
+	mov	eax, edi
+	mov	bl, 160
+	div	bl
+	and	eax, 0FFh
+	inc	eax
+	mov	bl, 160
+	mul	bl
+	mov	edi, eax
+	pop	eax
+	jmp	.1
+.3:
+	mov	[gs:edi], ax
+	add	edi, 2
+	jmp	.1
+
+.2:
+	mov	[dwDispPos], edi
+
+	pop	edi
+	pop	esi
+	pop	ebx
+	pop	ebp
+	ret
+; DispStr 结束------------------------------------------------------------
+
+; ------------------------------------------------------------------------
+; 换行
+; ------------------------------------------------------------------------
+DispReturn:
+	push	szReturn
+	call	DispStr			;printf("\n");
+	add	esp, 4
+
+	ret
+; DispReturn 结束---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; 内存拷贝，仿 memcpy
+; ------------------------------------------------------------------------
+; void* MemCpy(void* es:pDest, void* ds:pSrc, int iSize);
+; ------------------------------------------------------------------------
+MemCpy:
+	push	ebp
+	mov	ebp, esp
+
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	edi, [ebp + 8]	; Destination
+	mov	esi, [ebp + 12]	; Source
+	mov	ecx, [ebp + 16]	; Counter
+.1:
+	cmp	ecx, 0		; 判断计数器
+	jz	.2		; 计数器为零时跳出
+
+	mov	al, [ds:esi]		; ┓
+	inc	esi			; ┃
+					; ┣ 逐字节移动
+	mov	byte [es:edi], al	; ┃
+	inc	edi			; ┛
+
+	dec	ecx		; 计数器减一
+	jmp	.1		; 循环
+.2:
+	mov	eax, [ebp + 8]	; 返回值
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+
+	ret			; 函数结束，返回
+; MemCpy 结束-------------------------------------------------------------
+
+; 显示内存信息 --------------------------------------------------------------
+DispMemInfo:
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	esi, MemChkBuf
+	mov	ecx, [dwMCRNumber];for(int i=0;i<[MCRNumber];i++)//每次得到一个ARDS
+.loop:				  ;{
+	mov	edx, 5		  ;  for(int j=0;j<5;j++)//每次得到一个ARDS中的成员
+	mov	edi, ARDStruct	  ;  {//依次显示:BaseAddrLow,BaseAddrHigh,LengthLow
+.1:				  ;               LengthHigh,Type
+	push	dword [esi]	  ;
+	call	DispInt		  ;    DispInt(MemChkBuf[j*4]); // 显示一个成员
+	pop	eax		  ;
+	stosd			  ;    ARDStruct[j*4] = MemChkBuf[j*4];
+	add	esi, 4		  ;
+	dec	edx		  ;
+	cmp	edx, 0		  ;
+	jnz	.1		  ;  }
+	call	DispReturn	  ;  printf("\n");
+	cmp	dword [dwType], 1 ;  if(Type == AddressRangeMemory)
+	jne	.2		  ;  {
+	mov	eax, [dwBaseAddrLow];
+	add	eax, [dwLengthLow];
+	cmp	eax, [dwMemSize]  ;    if(BaseAddrLow + LengthLow > MemSize)
+	jb	.2		  ;
+	mov	[dwMemSize], eax  ;    MemSize = BaseAddrLow + LengthLow;
+.2:				  ;  }
+	loop	.loop		  ;}
+				  ;
+	call	DispReturn	  ;printf("\n");
+	push	szRAMSize	  ;
+	call	DispStr		  ;printf("RAM size:");
+	add	esp, 4		  ;
+				  ;
+	push	dword [dwMemSize] ;
+	call	DispInt		  ;DispInt(MemSize);
+	add	esp, 4		  ;
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	ret
+; ---------------------------------------------------------------------------
+
+; 启动分页机制 --------------------------------------------------------------
+SetupPaging:
+	; 根据内存大小计算应初始化多少PDE以及多少页表
+	xor	edx, edx
+	mov	eax, [dwMemSize]
+	mov	ebx, 400000h	; 400000h = 4M = 4096 * 1024, 一个页表对应的内存大小
+	div	ebx
+	mov	ecx, eax	; 此时 ecx 为页表的个数，也即 PDE 应该的个数
+	test	edx, edx
+	jz	.no_remainder
+	inc	ecx		; 如果余数不为 0 就需增加一个页表
+.no_remainder:
+	push	ecx		; 暂存页表个数
+
+	; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
+
+	; 首先初始化页目录
+	mov	ax, SelectorFlatRW
+	mov	es, ax
+	mov	edi, PageDirBase	; 此段首地址为 PageDirBase
+	xor	eax, eax
+	mov	eax, PageTblBase | PG_P  | PG_USU | PG_RWW
+.1:
+	stosd
+	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
+	loop	.1
+
+	; 再初始化所有页表
+	pop	eax			; 页表个数
+	mov	ebx, 1024		; 每个页表 1024 个 PTE
+	mul	ebx
+	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
+	mov	edi, PageTblBase	; 此段首地址为 PageTblBase
+	xor	eax, eax
+	mov	eax, PG_P  | PG_USU | PG_RWW
+.2:
+	stosd
+	add	eax, 4096		; 每一页指向 4K 的空间
+	loop	.2
+
+	mov	eax, PageDirBase
+	mov	cr3, eax
+	mov	eax, cr0
+	or	eax, 80000000h
+	mov	cr0, eax
+	jmp	short .3
+.3:
+	nop
+
+	ret
+; 分页机制启动完毕 ----------------------------------------------------------
+
+
+; SECTION .data1 之开始 ---------------------------------------------------------------------------------------------
+[SECTION .data1]
+
+ALIGN	32
+
+LABEL_DATA:
+; 实模式下使用这些符号
+; 字符串
+_szMemChkTitle:	db "BaseAddrL BaseAddrH LengthLow LengthHigh   Type", 0Ah, 0
+_szRAMSize:	db "RAM size:", 0
+_szReturn:	db 0Ah, 0
+;; 变量
+_dwMCRNumber:	dd 0	; Memory Check Result
+_dwDispPos:	dd (80 * 6 + 0) * 2	; 屏幕第 6 行, 第 0 列。
+_dwMemSize:	dd 0
+_ARDStruct:	; Address Range Descriptor Structure
+  _dwBaseAddrLow:		dd	0
+  _dwBaseAddrHigh:		dd	0
+  _dwLengthLow:			dd	0
+  _dwLengthHigh:		dd	0
+  _dwType:			dd	0
+_MemChkBuf:	times	256	db	0
+;
+;; 保护模式下使用这些符号
+szMemChkTitle		equ	BaseOfLoaderPhyAddr + _szMemChkTitle
+szRAMSize		equ	BaseOfLoaderPhyAddr + _szRAMSize
+szReturn		equ	BaseOfLoaderPhyAddr + _szReturn
+dwDispPos		equ	BaseOfLoaderPhyAddr + _dwDispPos
+dwMemSize		equ	BaseOfLoaderPhyAddr + _dwMemSize
+dwMCRNumber		equ	BaseOfLoaderPhyAddr + _dwMCRNumber
+ARDStruct		equ	BaseOfLoaderPhyAddr + _ARDStruct
+	dwBaseAddrLow	equ	BaseOfLoaderPhyAddr + _dwBaseAddrLow
+	dwBaseAddrHigh	equ	BaseOfLoaderPhyAddr + _dwBaseAddrHigh
+	dwLengthLow	equ	BaseOfLoaderPhyAddr + _dwLengthLow
+	dwLengthHigh	equ	BaseOfLoaderPhyAddr + _dwLengthHigh
+	dwType		equ	BaseOfLoaderPhyAddr + _dwType
+MemChkBuf		equ	BaseOfLoaderPhyAddr + _MemChkBuf
+
+
+; 堆栈就在数据段的末尾
+StackSpace:	times	1024	db	0
+TopOfStack	equ	BaseOfLoaderPhyAddr + $	; 栈顶
+; SECTION .data1 之结束 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
